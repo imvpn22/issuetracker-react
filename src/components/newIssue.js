6e81fd0ce3b74704c3fb0330dfa6501d4c4ca548
Vipin Yadav <imvpn22@gmail.com>
2019-04-10 20:20:51 +0530

    Updated New Issue view

--- src/components/newIssue.js
+++ src/components/newIssue.js
@@ -1,48 +1,61 @@
 import React from 'react';
+import Moment from 'moment';
 
 import { BASE_URL } from '../config';
 
-const API_URL = `${BASE_URL}/threads`;
+const API_URL = `${BASE_URL}/issues`;
 
-class NewThread extends React.Component {
+class NewIssue extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: '',
-      description: '',
-      tags: ''
+      issue: {}
     }
   }
 
-  handleTitleChange = (event) => {
-    this.setState({title: event.target.value});
-  };
-
   handleDescriptionChange = (event) => {
-    this.setState({description: event.target.value});
+    let issue = this.state.issue;
+    issue.description = event.target.value;
+    this.setState({ issue });
   };
 
-  handleTagsChange = (event) => {
-    this.setState({tags: event.target.value});
+  handleSeverityChange = (event) => {
+    let issue = this.state.issue;
+    issue.severity = event.target.value;
+    this.setState({ issue });
+  };
+
+  handleStatusChange = (event) => {
+    let issue = this.state.issue;
+    issue.status = event.target.value;
+    this.setState({ issue });
+  };
+
+  handleCreatedDateChange = (event) => {
+    let issue = this.state.issue;
+    issue.createdDate = event.target.value;
+    this.setState({ issue });
+  };
+
+  handleResolvedDateChange = (event) => {
+    let issue = this.state.issue;
+    issue.resolvedDate = event.target.value;
+    this.setState({ issue });
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
 
-    let thread = {
-      title: this.state.title,
-      description: this.state.description,
-      tags: this.state.tags.split(',')
-    }
+    let issue = this.state.issue;
 
-    fetch(API_URL, {
+    fetch(`${API_URL}`, {
         method: 'post',
         headers: { 'Content-type': 'application/json' },
-        body: JSON.stringify(thread)
+        body: JSON.stringify(issue)
       }
     ).then(res => res.json())
-    .then(thread => {
-      this.props.history.push('/');
+    .then(newIssue => {
+      this.props.history.push(`/issue/${newIssue._id}/view`);
     }).catch(err => {
       console.log(err);
     });
@@ -54,26 +67,11 @@ class NewThread extends React.Component {
       <div className='container'>
 
         <form
-          className='form form-horizontal new-thread-form'
+          className='form form-horizontal new-issue-form issue-card'
           onSubmit={e => this.handleSubmit(e)}>
 
           <div className='form-group'>
-            <h4>New Thread</h4>
-          </div>
-
-          <div className='form-group'>
-            <label className='control-label col-sm-3' htmlFor='title'>
-              Title
-            </label>
-            <input
-              className='col-sm-9'
-              required
-              type='text'
-              id='title'
-              value={this.state.title}
-              onChange={e => this.handleTitleChange(e)}
-              placeholder='Enter Title of the Thread'
-            />
+            <h4>New Issue</h4>
           </div>
 
           <div className='form-group'>
@@ -81,38 +79,95 @@ class NewThread extends React.Component {
               Description
             </label>
             <textarea
+              rows='4'
               className='col-sm-9'
               required
               id='description'
-              value={this.state.description}
+              value={this.state.issue.description}
               onChange={e => this.handleDescriptionChange(e)}
-              placeholder='Enter Description of the Thread'
+              placeholder='Enter Description of the Issue'
              />
           </div>
 
           <div className='form-group'>
-            <label className='control-label col-sm-3' htmlFor='tags'>
-              Tags
+            <label className='control-label col-sm-3' htmlFor='severity'>
+              Severity
             </label>
-            <input
+            <select
               className='col-sm-9'
               required
-              type='text'
-              id='tags'
-              value={this.state.tags}
-              onChange={e => this.handleTagsChange(e)}
-              placeholder='Enter Tags (Comma separated)'
-            />
+              id='severity'
+              value={this.state.issue.severity}
+              onChange={e => this.handleSeverityChange(e)}>
+              <option value=''>Select an option</option>
+              <option value='Critical'>Critical</option>
+              <option value='Major'>Major</option>
+              <option value='Minor'>Minor</option>
+             </select>
           </div>
 
           <div className='form-group'>
-           <button className="btn btn-primary" type='submit' value='Create'> Create </button>
+            <label className='control-label col-sm-3' htmlFor='status'>
+              Status
+            </label>
+            <select
+              className='col-sm-9'
+              required
+              id='status'
+              value={this.state.issue.status}
+              onChange={e => this.handleStatusChange(e)}>
+              <option value=''>Select an option</option>
+              <option value='Open'>Open</option>
+              <option value='In Progress'>In Progress</option>
+              <option value='Closed'>Closed</option>
+             </select>
+          </div>
+
+          <div className='form-group'>
+            <label className='control-label col-sm-3' htmlFor='createdDate'>
+              Created Date
+            </label>
+            <input
+              type='date'
+              className='col-sm-9'
+              required
+              id='createdDate'
+              value={Moment(this.state.issue.createdDate).format('YYYY-MM-DD')}
+              onChange={e => this.handleCreatedDateChange(e)}
+             />
+          </div>
+
+          <div className='form-group'>
+            <label className='control-label col-sm-3' htmlFor='resolvedDate'>
+              Resolved Date
+            </label>
+            <input
+              type='date'
+              className='col-sm-9'
+              required
+              id='resolvedDate'
+              value={Moment(this.state.issue.resolvedDate).format('YYYY-MM-DD')}
+              onChange={e => this.handleResolvedDateChange(e)}
+             />
+          </div>
+
+          <div className='form-group'>
+            <span className='col-sm-4'>
+              <button className="btn btn-primary" type='submit'>
+                Create
+              </button>
+            </span>
           </div>
         </form>
+        <span className='col-sm-offset-6 col-sm-8'>
+          <a href={'/'}>
+            <button className='btn btn-danger'> Home </button>
+          </a>
+        </span>
 
       </div>
     )
   }
 }
 
-export default NewThread;
+export default NewIssue;
